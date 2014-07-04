e47dcb49a623bfba9552b3c97c2131aa72875656
Michael <mgb45@hotspur.(none)>
2014-07-04 15:14:18 +0100

    Added cross validating results

---
+++ bin/runSamplingTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /opt/ros/groovy/setup.bash
+source /home/mgb45/Documents/setup.bash
+
+N=100000
+K=27
+
+for ((i=0; i<20; i=$i+1));
+do
+	roslaunch mkfbodytracker bodyTrackingBagCompare.launch "file1:=/data13D_${N}_${K}.yml" "file2:=/data23D_${N}_${K}.yml"
+done
