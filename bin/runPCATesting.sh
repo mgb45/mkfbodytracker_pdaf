cadc916e07c4659641bf3dfdad13aa12a31d15d8
Michael <mgb45@hotspur.(none)>
2014-07-08 11:39:48 +0100

    Preparing for pca mass result run

--- bin/runPCATesting.sh
+++ bin/runPCATesting.sh
@@ -4,10 +4,12 @@ source /opt/ros/groovy/setup.bash
 source /home/mgb45/Documents/setup.bash
 
 N=100000
-K=(1 3 6 11 17 22 30 26 22 23 21 24 29 37 37 47 42 36 40 38)
+#K=(1 3 6 11 17 22 30 26 22 23 21 24 29 37 37 47 42 36 40 38)
 
-for ((i=0; i<20; i=$i+1));
+for ((k=1; k<50; k=$k+1));
 do
-	d=$((i+1))
-	roslaunch mkfbodytracker bodyTrackingBagCompare.launch "file1:=/data13D_PCA_${N}_${K[${i}]}_$d.yml" "file2:=/data23D_PCA_${N}_${K[${i}]}_$d.yml"
+	for ((i=1; i<21; i=$i+1));
+	do
+		roslaunch mkfbodytracker bodyTrackingBagCompare.launch "file1:=/data13D_PCA_${N}_${k}_$i.yml" "file2:=/data23D_PCA_${N}_${k}_${i}.yml"
+	done
 done
