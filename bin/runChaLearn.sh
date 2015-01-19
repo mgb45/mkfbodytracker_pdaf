a5a73274e8e426f3c4ef224f43a22707da2a1af0
mgb45 <mgb45@cam.ac.uk>
2015-01-19 16:09:35 +0000

    Added chalearn launch files and scripts

---
+++ bin/runChaLearn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in /media/mgb45/System/Data/ChaLearn/valid* ; do
+	for j in $i/* ; do
+		rm -fr $j/Sample${j:52:5}_color*.bag
+		roslaunch mkfbodytracker_pdaf ChaLearn.launch FILENAME:=$j/Sample${j:52:5}_color SAVE:=true
+	done
+done
